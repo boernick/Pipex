25eb915677ae5b4bb9010f8b0c966e1e60d39e1d
Nick Boer <nboer@c3a4c9.42berlin.de>
2024-09-22 18:47:26 +0200

    final

--- Makefile
+++ Makefile
@@ -1,1 +1,44 @@
-test
+NAME = pipex
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+SRCS = 
+OBJS = $(SRCS:.c=.o)
+HEADER = pipex.h
+
+PRINTF_DIR = includes/ft_printf
+PRINTF = $(PRINTF_DIR)/libftprintf.a
+LIBFT_DIR = includes/Libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+INCLUDES = -I $(PRINTF_DIR) -I $(LIBX_DIR) -I $(LIBFT_DIR)
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(PRINTF) $(LIBFT) $(LIBX)
+	$(CC) $(CFLAGS) $(INCLUDES) $(OBJS) $(PRINTF) $(LIBFT) -o $(NAME)
+
+%.o: %.c $(HEADER)
+	$(CC) $(CFLAGS) $(INCLUDES) -g -c $< -o $@
+
+$(LIBFT): 
+	make -C $(LIBFT_DIR) bonus
+
+$(PRINTF): $(LIBFT)
+	make -C $(PRINTF_DIR)
+
+$(LIBX):
+	make -C $(LIBX_DIR)
+
+clean:
+	rm -f $(OBJS)
+	make -C $(PRINTF_DIR) clean
+	make -C $(LIBFT_DIR) clean
+
+fclean: clean
+	rm -f $(NAME)
+	make -C $(PRINTF_DIR) fclean
+	make -C $(LIBFT_DIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 18:41:06 by nboer             #+#    #+#             */
+/*   Updated: 2024/09/22 18:47:17 by nboer            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "/home/nboer/git/Pipex/includes/ft_printf/ft_printf.h"
+#include "/home/nboer/git/Pipex/includes/Libft/libft.h" 
+
+int	main(void)
+{
+
+	return (0);
+}
