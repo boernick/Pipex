bd0b32b0755c795b760dca207d916b41f91d0cbe
boernick <nick.boer@hotmail.com>
2024-10-01 17:24:41 +0200

    updateoct1

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 18:40:48 by nboer             #+#    #+#             */
-/*   Updated: 2024/09/26 18:24:24 by nboer            ###   ########.fr       */
+/*   Updated: 2024/10/01 17:21:23 by nick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,37 +27,43 @@ char	*find_ex(char *arg, char **path_env)
 	return (path_split[0]);
 }
 
-int	run_child(char *arg)
+int	run_child(char *arg, int *fd, char **path_env)
 {
 	ft_printf("child opens %s", arg);
-	find_ex(arg, getenv("PATH"));
+	close(fd[0]);
+	find_ex(arg, path_env);
 	return (0);
 }
 
-int	run_parent(char *arg)
+int	run_parent(char *arg, int *fd, char **path_env)
 {
 	ft_printf("parent reads %s", arg);
-	// find_ex(arg);
+	close(fd[1]);
+	// duplicate file descriptor into 
+	dup2(fd[0], STDIN_FILENO);
+	find_ex(arg, path_env);
 	return (0);
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **env)
 {
 	pid_t	pid1;
 	int		fd[2];
 
 	argv[2] = NULL;
+    // run pipe to create two file descriptors: one for reading and one for writing.
 	if (argc == 5)
 	{
 		if (pipe(fd) == -1)
 			return (1);
+        // fork the process: this duplicates the process. The childprocess (pid = 0) will handle one part of the pipeline, the parent the other.
 		pid1 = fork();
 		if (pid1 < 0)
 			str_error("FORK ERROR");
 		if (pid1 == 0)
-			run_child(argv[4]);
+			run_child(argv[4], fd, env);
 		waitpid(pid1, NULL, 0);
-		run_parent(argv[1]);
+		run_parent(argv[1], fd, env);
 		ft_printf(" FINISH %i \n", pid1);
 	}
 	else
--- pipex.h
+++ pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 18:41:06 by nboer             #+#    #+#             */
-/*   Updated: 2024/09/26 17:56:12 by nboer            ###   ########.fr       */
+/*   Updated: 2024/10/01 17:18:34 by nick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <stdio.h>
-# include "/home/nboer/git/Pipex/includes/ft_printf/ft_printf.h"
-# include "/home/nboer/git/Pipex/includes/Libft/libft.h"
+# include "/home/nick/42Berlin/Pipex/includes/ft_printf/ft_printf.h"
+# include "/home/nick/42Berlin/Pipex/includes/Libft/libft.h"
+// # include "/home/nboer/git/Pipex/includes/ft_printf/ft_printf.h"
+// # include "/home/nboer/git/Pipex/includes/Libft/libft.h"
 
 int	str_error(char *error);
 
